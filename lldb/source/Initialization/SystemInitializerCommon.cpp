d6b6095211265fe3f8d78c8a32089d6349df9420
KENZ <KENZ.gelsoft@gmail.com>
2023-07-05 05:44:38 +0900

    Use POSIX logging for Haiku too

--- lldb/source/Initialization/SystemInitializerCommon.cpp
+++ lldb/source/Initialization/SystemInitializerCommon.cpp
@@ -18,7 +18,7 @@
 #include "lldb/Utility/Timer.h"
 #include "lldb/lldb-private.h"
 
-#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__HAIKU__)
 #include "Plugins/Process/POSIX/ProcessPOSIXLog.h"
 #endif
 
@@ -135,7 +135,7 @@ llvm::Error SystemInitializerCommon::Initialize() {
 
   process_gdb_remote::ProcessGDBRemoteLog::Initialize();
 
-#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__HAIKU__)
   ProcessPOSIXLog::Initialize();
 #endif
 #if defined(_WIN32)
