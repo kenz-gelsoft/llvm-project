e19c9b38bd6bbd0364b7950f7fc014d088cf4b3b
KENZ <KENZ.gelsoft@gmail.com>
2023-07-06 05:51:24 +0900

    Don't define real time signals for now on Haiku

--- lldb/source/Plugins/Process/Utility/HaikuSignals.cpp
+++ lldb/source/Plugins/Process/Utility/HaikuSignals.cpp
@@ -49,37 +49,5 @@ void HaikuSignals::Reset() {
   AddSignal(30,     "SIGBUS",       false,    true,   true,   "access to undefined portion of a memory object");
   AddSignal(31,     "SIGRESERVED1", false,    false,  false,  "reserved for future use");
   AddSignal(32,     "SIGRESERVED2", false,    false,  false,  "reserved for future use");
-  AddSignal(33,     "SIG33",        false,    false,  false,  "threading library internal signal 2");
-  AddSignal(34,     "SIGRTMIN",     false,    false,  false,  "real time signal 0");
-  AddSignal(35,     "SIGRTMIN+1",   false,    false,  false,  "real time signal 1");
-  AddSignal(36,     "SIGRTMIN+2",   false,    false,  false,  "real time signal 2");
-  AddSignal(37,     "SIGRTMIN+3",   false,    false,  false,  "real time signal 3");
-  AddSignal(38,     "SIGRTMIN+4",   false,    false,  false,  "real time signal 4");
-  AddSignal(39,     "SIGRTMIN+5",   false,    false,  false,  "real time signal 5");
-  AddSignal(40,     "SIGRTMIN+6",   false,    false,  false,  "real time signal 6");
-  AddSignal(41,     "SIGRTMIN+7",   false,    false,  false,  "real time signal 7");
-  AddSignal(42,     "SIGRTMIN+8",   false,    false,  false,  "real time signal 8");
-  AddSignal(43,     "SIGRTMIN+9",   false,    false,  false,  "real time signal 9");
-  AddSignal(44,     "SIGRTMIN+10",  false,    false,  false,  "real time signal 10");
-  AddSignal(45,     "SIGRTMIN+11",  false,    false,  false,  "real time signal 11");
-  AddSignal(46,     "SIGRTMIN+12",  false,    false,  false,  "real time signal 12");
-  AddSignal(47,     "SIGRTMIN+13",  false,    false,  false,  "real time signal 13");
-  AddSignal(48,     "SIGRTMIN+14",  false,    false,  false,  "real time signal 14");
-  AddSignal(49,     "SIGRTMIN+15",  false,    false,  false,  "real time signal 15");
-  AddSignal(50,     "SIGRTMAX-14",  false,    false,  false,  "real time signal 16"); // switching to SIGRTMAX-xxx to match "kill -l" output
-  AddSignal(51,     "SIGRTMAX-13",  false,    false,  false,  "real time signal 17");
-  AddSignal(52,     "SIGRTMAX-12",  false,    false,  false,  "real time signal 18");
-  AddSignal(53,     "SIGRTMAX-11",  false,    false,  false,  "real time signal 19");
-  AddSignal(54,     "SIGRTMAX-10",  false,    false,  false,  "real time signal 20");
-  AddSignal(55,     "SIGRTMAX-9",   false,    false,  false,  "real time signal 21");
-  AddSignal(56,     "SIGRTMAX-8",   false,    false,  false,  "real time signal 22");
-  AddSignal(57,     "SIGRTMAX-7",   false,    false,  false,  "real time signal 23");
-  AddSignal(58,     "SIGRTMAX-6",   false,    false,  false,  "real time signal 24");
-  AddSignal(59,     "SIGRTMAX-5",   false,    false,  false,  "real time signal 25");
-  AddSignal(60,     "SIGRTMAX-4",   false,    false,  false,  "real time signal 26");
-  AddSignal(61,     "SIGRTMAX-3",   false,    false,  false,  "real time signal 27");
-  AddSignal(62,     "SIGRTMAX-2",   false,    false,  false,  "real time signal 28");
-  AddSignal(63,     "SIGRTMAX-1",   false,    false,  false,  "real time signal 29");
-  AddSignal(64,     "SIGRTMAX",     false,    false,  false,  "real time signal 30");
   // clang-format on
 }
